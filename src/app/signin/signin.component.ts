e2f7accee9234b6560df52bd48e7d300a3df718d
Harisudhan <m.harisudhan95@gmail.com>
2018-07-09 01:37:37 +0530

    Initial Commit

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,44 @@
+import { 
+  Component, 
+  OnInit, 
+  ViewChild 
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  @ViewChild('f') form: NgForm;
+  error: string;
+
+  constructor( 
+    private authService: AuthService,
+    private router: Router
+   ) { 
+    this.error = null;
+  }
+
+  ngOnInit() {
+  }
+
+  onSignIn() {
+    this.authService.signInUser(this.form.value.email, this.form.value.password)
+      .then((user) => {
+        console.log(user.user)
+        this.authService.setLoginStatus(true);
+        this.router.navigateByUrl('home');
+      })
+      .catch( err => this.error = err.message );
+    this.form.reset();
+  }
+
+  removeError() {
+    this.error = null;
+  }
+}
